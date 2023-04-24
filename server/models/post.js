e4d116c15df2dbf70c7531c2fb9cc00ee6e8e8b7
Sai Vittal <vickysaivittal@gmail.com>
2023-04-24 13:24:18 +0530

    first Commit

---
+++ server/models/post.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    location: String,
+    description: String,
+    picturePath: String,
+    userPicturePath: String,
+    liked: {
+      type: Map,
+      of: Boolean,
+    },
+    commments: {
+      type: Array,
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
